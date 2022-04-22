f26f2b10a5d2b634b0a739a2a9214505f1ffaa8a
dontTouchTh1s <alim201381@gmail.com>
2022-04-22 21:39:15 +0430

    Initial commit

---
+++ accounting/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace accounting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string _amount;
+        private readonly SqlConnect _sql;
+
+        public MainWindow()
+        {
+            _sql = new();
+            _sql.Open();
+            SqlDataReader data = _sql.Query("SELECT amount FROM fund");
+            while (data.Read())
+            {
+                _amount = data["amount"].ToString();
+            }
+
+            data.Close();
+
+            InitializeComponent();
+            TBamount.Text = _amount;
+        }
+
+        private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void textBox_Copy2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private async void create_account(object sender, RoutedEventArgs e)
+        {
+            string name = TbName.Text;
+            string lastName = TbLastName.Text;
+            string fatherName = TbFatherName.Text;
+            string nationalId = TbNationalId.Text;
+            string accountNumber = TbAccountNumber.Text;
+            string accountId = "";
+            await _sql.Query(
+                "INSERT INTO people (national_id, name, last_name, father_name, self_account_number) VALUES (@0, @1, @2, @3, @4)",
+                name, lastName, fatherName, nationalId, accountNumber).CloseAsync();
+            var reader = _sql.Query("SELECT account_id FROM people WHERE national_id = @0", nationalId);
+            while (reader.Read())
+            {
+                accountId = reader["account_id"].ToString();
+            }
+            await reader.CloseAsync();
+
+            var toDay = DateTime.Today.ToString();
+            await _sql.Query("INSERT INTO accounts VALUES (@0, @1, @2)", accountId, "0", toDay).CloseAsync();
+
+
+            LblMessage.Content = "حساب با موفقیت ساخته شد";
+            LblMessage.Visibility = Visibility.Visible;
+            await Animation_Label();
+        }
+
+        private async Task Animation_Label()
+        {
+            await Task.Delay(2000);
+            DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
+            LblMessage.BeginAnimation(Label.OpacityProperty, animation);
+        }
+    }
+}
---
+++ accounting/SQL.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace accounting
+{
+    public class SqlConnect
+    {
+        private const string ConnectionString = "Server=ALIM;Database=accounting;Trusted_Connection=True;";
+        private SqlConnection _connection;
+        public void Open()
+        {
+            _connection = new(ConnectionString);
+            _connection.Open();
+        }
+        public void Close()
+        {
+            _connection.Close();
+        }
+        public SqlDataReader Query(string query, params string[] values)
+        {
+
+            SqlCommand command = new(query, _connection);
+            for (int i = 0; i < values.Length; i++)
+            {
+                command.Parameters.AddWithValue("@" + i.ToString(), values[i]);
+            }
+            SqlDataReader reader = command.ExecuteReader();
+            return reader;
+        }
+
+    }
+}
