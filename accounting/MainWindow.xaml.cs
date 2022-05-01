f752ab834ea5300751755ca9b46e591fa3156641
dontTouchTh1s <alim201381@gmail.com>
2022-05-01 12:45:59 +0430

    Use async sql calls

--- accounting/MainWindow.xaml.cs
+++ accounting/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,26 +7,31 @@ using System.Windows.Media.Animation;
 namespace accounting
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly string _amount;
-        private readonly SqlConnect _sql;
+        private string _amount;
+        private SqlConnect _sql;
 
         public MainWindow()
         {
-            _sql = new();
-            _sql.Open();
-            SqlDataReader data = _sql.Query("SELECT amount FROM fund");
-            while (data.Read())
-            {
-                _amount = data["amount"].ToString();
-            }
-
-            data.Close();
-
             InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Connect to data base when form loaded
+        /// </summary>
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sql = new SqlConnect();
+            await _sql.Open();
+
+            var data = await _sql.Query("SELECT amount FROM fund");
+            while (data.Read())
+                _amount = data["amount"].ToString();
+            await data.CloseAsync();
+
             TBamount.Text = _amount;
         }
 
@@ -43,26 +47,27 @@ namespace accounting
         {
         }
 
-        private async void create_account(object sender, RoutedEventArgs e)
+        private async void CreateAccount(object sender, RoutedEventArgs e)
         {
-            string name = TbName.Text;
-            string lastName = TbLastName.Text;
-            string fatherName = TbFatherName.Text;
-            string nationalId = TbNationalId.Text;
-            string accountNumber = TbAccountNumber.Text;
-            string accountId = "";
-            await _sql.Query(
+            var name = FgName.TextBox.Text;
+            var lastName = FgLastName.TextBox.Text;
+            var fatherName = FgFatherName.TextBox.Text;
+            var nationalId = FgNationalId.TextBox.Text;
+            var accountNumber = FgSelfAccountNumber.TextBox.Text;
+            var accountId = "";
+
+            var reader = await _sql.Query(
                 "INSERT INTO people (national_id, name, last_name, father_name, self_account_number) VALUES (@0, @1, @2, @3, @4)",
-                name, lastName, fatherName, nationalId, accountNumber).CloseAsync();
-            var reader = _sql.Query("SELECT account_id FROM people WHERE national_id = @0", nationalId);
-            while (reader.Read())
-            {
-                accountId = reader["account_id"].ToString();
-            }
+                name, lastName, fatherName, nationalId, accountNumber);
+            await reader.CloseAsync();
+
+            reader = await _sql.Query("SELECT account_id FROM people WHERE national_id = @0", nationalId);
+            while (reader.Read()) accountId = reader["account_id"].ToString();
             await reader.CloseAsync();
 
             var toDay = DateTime.Today.ToString();
-            await _sql.Query("INSERT INTO accounts VALUES (@0, @1, @2)", accountId, "0", toDay).CloseAsync();
+            reader = await _sql.Query("INSERT INTO accounts VALUES (@0, @1, @2)", accountId, "0", toDay);
+            await reader.CloseAsync();
 
 
             LblMessage.Content = "حساب با موفقیت ساخته شد";
@@ -72,9 +77,9 @@ namespace accounting
 
         private async Task Animation_Label()
         {
-            await Task.Delay(2000);
-            DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
-            LblMessage.BeginAnimation(Label.OpacityProperty, animation);
+            await Task.Delay(3000);
+            var animation = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
+            LblMessage.BeginAnimation(OpacityProperty, animation);
         }
     }
 }
