3712bfad84d56af115b1ee61ad000ded0dc2f5da
dontTouchTh1s <alim201381@gmail.com>
2022-05-02 13:38:04 +0430

    Add TextBox view model

---
+++ accounting/TextBoxViewModel.cs
@@ -0,0 +1,14 @@
+﻿#nullable enable
+namespace accounting
+{
+    public class TextBoxViewModel
+    {
+        private string? _name;
+
+        public string? Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+    }
+}
