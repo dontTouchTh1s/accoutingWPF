583d41e5a912a122d235a554fcece452f8c9fc39
dontTouchTh1s <alim201381@gmail.com>
2022-05-04 16:05:05 +0430

    Add numeric ValidationRule

--- accounting/Validation/NumericValidationRule.cs
+++ accounting/Validation/NumericValidationRule.cs
@@ -3,14 +3,14 @@ using System.Windows.Controls;
 
 namespace accounting.Validation
 {
-    public class NotEmptyValidationRule : ValidationRule
+    public class NumericValidationRule : ValidationRule
     {
         public override ValidationResult Validate(object? value, CultureInfo cultureInfo)
         {
             return value == null
                 ? ValidationResult.ValidResult
-                : string.IsNullOrWhiteSpace(value.ToString())
-                    ? new ValidationResult(false, "فیلد را پر کنید.")
+                : !int.TryParse(value.ToString(), out _)
+                    ? new ValidationResult(false, "فقط عدد میتوانید وارد کنید.")
                     : ValidationResult.ValidResult;
         }
     }
