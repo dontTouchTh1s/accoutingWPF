c2b2b6cdfe108831802191c103f9fae21eac53b4
dontTouchTh1s <alim201381@gmail.com>
2022-05-04 16:00:39 +0430

    Fix null value of validation text

--- accounting/Validation/NotEmptyValidationRule.cs
+++ accounting/Validation/NotEmptyValidationRule.cs
@@ -1,13 +1,15 @@
 ﻿using System.Globalization;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace accounting.Validation
 {
     public class NotEmptyValidationRule : ValidationRule
     {
-        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        public override ValidationResult Validate(object? value, CultureInfo cultureInfo)
         {
-            return string.IsNullOrWhiteSpace(value.ToString())
+            return value == null ? ValidationResult.ValidResult :
+            string.IsNullOrWhiteSpace((value).ToString())
                 ? new ValidationResult(false, "فیلد را پر کنید.")
                 : ValidationResult.ValidResult;
         }
