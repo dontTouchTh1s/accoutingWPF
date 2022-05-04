ab1511d85e149630e35b5107bc2148242b774f9b
dontTouchTh1s <alim201381@gmail.com>
2022-05-04 16:20:36 +0430

    Add alphabetical validation rule

---
+++ accounting/Validation/AlphabeticalValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace accounting.Validation
+{
+    public class AlphabeticalValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object? value, CultureInfo cultureInfo)
+        {
+            return value == null
+                ? ValidationResult.ValidResult
+                : !Regex.IsMatch(value.ToString()!, @"^[a-zA-Z]+$")
+                    ? new ValidationResult(false, "فقط حروف میتوانید وارد کنید..")
+                    : ValidationResult.ValidResult;
+        }
+    }
+}
