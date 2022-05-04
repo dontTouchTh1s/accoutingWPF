25de7615528cc7269bb53824a703d61d55f3b651
dontTouchTh1s <alim201381@gmail.com>
2022-05-04 11:20:13 +0430

    Update property when sets

---
+++ accounting/ViewModels/TextBoxViewModel.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+namespace accounting.ViewModels
+{
+    public class TextBoxViewModel : BaseViewModel
+    {
+        private string? _name;
+        private string? _name2;
+
+        public string? Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+        public string? Name2
+        {
+            get => _name2;
+            set => SetProperty(ref _name2, value);
+        }
+    }
+}
