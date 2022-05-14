4560b5377c0f793213281948e1411afc8cc4275d
dontTouchTh1s <alim201381@gmail.com>
2022-05-14 20:04:18 +0430

    Initial dto converter to convert model to dto

---
+++ accounting/Services/DTOConverter.cs
@@ -0,0 +1,32 @@
+﻿using accounting.DTOs;
+using accounting.Models;
+
+namespace accounting.Services
+{
+    public class DTOConverter
+    {
+        public AccountDTO AccountModelToDTO(AccountsModel accountsModel)
+        {
+            PeopleDTO owner = PeopleModelToDTO(accountsModel.Owner);
+            return new AccountDTO
+            {
+                AccountId = accountsModel.AccountId,
+                Credit = accountsModel.Credit,
+                Owner = owner,
+                CreateDate = accountsModel.CreateDate
+            };
+        }
+
+        public PeopleDTO PeopleModelToDTO(PeoplesModel peoplesModel)
+        {
+            return new PeopleDTO
+            {
+                NationalId = peoplesModel.NationalId,
+                Name = peoplesModel.Name,
+                LastName = peoplesModel.LastName,
+                FatherName = peoplesModel.FatherName,
+                PersonalAccountNumber = peoplesModel.PersonalAccountNumber
+            };
+        }
+    }
+}
