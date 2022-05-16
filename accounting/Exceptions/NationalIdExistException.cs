d25a493b08c11eac9b30ac5c9703b7517daa8d65
dontTouchTh1s <alim201381@gmail.com>
2022-05-16 16:26:46 +0430

    Add customizable messageDialog

---
+++ accounting/Exceptions/NationalIdExistException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace accounting.Exceptions
+{
+    class NationalIdExistException : Exception
+    {
+        public NationalIdExistException() { }
+        public NationalIdExistException(string nationalId) : base(string.Format("People with national id {0} currently exist", nationalId))
+        { }
+    }
+}
