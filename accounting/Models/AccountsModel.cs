c016b286cd47f0b638e048e09524f2623dec40fb
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:49:16 +0430

    Add availableCredit properties Add another constructor for DTOConverter, to get credit, availableCredit and time from DTO

--- accounting/Models/AccountsModel.cs
+++ accounting/Models/AccountsModel.cs
@@ -8,17 +8,36 @@ namespace accounting.Models
     {
         private readonly DataBaseAccountsServices _dataBaseAccountsServices;
 
-        public AccountsModel(int id, string ownerNationalId, DataBaseAccountsServices dataBaseAccountsServices)
+        public AccountsModel(int id,
+            int credit,
+            int availableCredit,
+            DateTime createDate,
+            string ownerNationalId,
+            DataBaseAccountsServices dataBaseAccountsServices)
+        {
+            _dataBaseAccountsServices = dataBaseAccountsServices;
+            Id = id;
+            CreateDate = createDate;
+            Credit = credit;
+            AvailableCredit = availableCredit;
+            OwnerNationalId = ownerNationalId;
+        }
+
+        public AccountsModel(int id,
+            string ownerNationalId,
+            DataBaseAccountsServices dataBaseAccountsServices)
         {
             _dataBaseAccountsServices = dataBaseAccountsServices;
             Id = id;
             CreateDate = DateTime.Now;
             Credit = 0;
+            AvailableCredit = 0;
             OwnerNationalId = ownerNationalId;
         }
 
         public int Id { get; }
         public int Credit { get; }
+        public int AvailableCredit { get; }
         public DateTime CreateDate { get; }
         public string OwnerNationalId { get; }
 
