8ba5d37c43d1b9efdceb51fc8e9a3319122eaa2d
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:34:12 +0430

    Remove alphabetical character from currency textboxes

---
+++ accounting/Commands/CreditPreviewKeyUpCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using accounting.ViewModels;
+
+namespace accounting.Commands
+{
+    public class CreditPreviewKeyUpCommand : BaseCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            var args = (KeyEventArgs)parameter!;
+            if (args.Key is >= Key.A and <= Key.Z)
+                args.Handled = true;
+        }
+    }
+}
