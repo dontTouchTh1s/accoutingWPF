9dcede99058484a5a270b470511eb068d213461e
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:32:09 +0430

    Create LendLoan Model

---
+++ accounting/Models/LoanModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace accounting.Models
+{
+    public class LoanModel
+    {
+        public LoanModel(int accountId, int amount, int installmentsCount, string? personalAccountNumber)
+        {
+            Amount = amount;
+            InstallmentsCount = installmentsCount;
+            LendDate = DateTime.Now;
+            AccountId = accountId;
+            PersonalAccountNumber = personalAccountNumber;
+        }
+
+        public int Id { get; }
+        public int Amount { get; }
+        public int InstallmentsCount { get; }
+        public DateTime LendDate { get; }
+        public int AccountId { get; }
+        public string? PersonalAccountNumber { get; }
+    }
+}
