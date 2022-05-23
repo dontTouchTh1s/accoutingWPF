574c19b7721c43f24ac1d948c72c5290c47081c0
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:33:34 +0430

    Call LendLoan From service in command

---
+++ accounting/Commands/LendLoandCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using accounting.Models;
+using accounting.ViewModels;
+using accounting.ViewModels.Dialogs;
+using MaterialDesignThemes.Wpf;
+
+namespace accounting.Commands
+{
+    public class LendLoanCommand : BaseAsyncCommand
+    {
+        private readonly InvestmentFundModel _investmentFundModel;
+        private readonly LendLoanViewModel _lendLoanViewModel;
+
+        public LendLoanCommand(LendLoanViewModel lendLoanViewModel, InvestmentFundModel investmentFundModel)
+        {
+            _lendLoanViewModel = lendLoanViewModel;
+            _investmentFundModel = investmentFundModel;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await _investmentFundModel.LendLoan(_lendLoanViewModel);
+            }
+            catch (Exception e)
+            {
+                var dialogViewModel =
+                    new MessageDialogViewModel("خطایی در ثبت اطالاعات رخ داده است.",
+                        PackIconKind.WarningCircle, new SolidColorBrush(Colors.Red));
+                await DialogHost.Show(dialogViewModel, "rootDialog");
+            }
+        }
+    }
+}
