5404a26078705b07ac33e1a027d0eae26392ceb3
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:52:46 +0430

    Create LoanViewModel

--- accounting/ViewModels/MainViewModel.cs
+++ accounting/ViewModels/MainViewModel.cs
@@ -9,10 +9,13 @@ namespace accounting.ViewModels
             TransactionsViewModel = new TransactionsViewModel(investmentFundModel);
             CreateAccountViewModel = new CreateAccountViewModel(investmentFundModel);
             SummeryViewModel = new SummeryViewModel(investmentFundModel);
+            LendLoanViewModel = new LendLoanViewModel(investmentFundModel);
         }
 
         public CreateAccountViewModel CreateAccountViewModel { get; }
         public SummeryViewModel SummeryViewModel { get; }
         public TransactionsViewModel TransactionsViewModel { get; }
+
+        public LendLoanViewModel LendLoanViewModel { get; }
     }
 }
