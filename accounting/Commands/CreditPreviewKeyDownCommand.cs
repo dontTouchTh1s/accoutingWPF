a3ee8c03d3a678b190a6889f073afbdf6ffa6032
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:34:51 +0430

    Reverse arrow keys functionality, cuz of rtf flowDirection

---
+++ accounting/Commands/CreditPreviewKeyDownCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace accounting.Commands
+{
+    public class CreditPreviewKeyDownCommand : BaseCommand
+    {
+        public override void Execute(object? parameter)
+        {
+            var args = (KeyEventArgs)parameter!;
+            switch (args.Key)
+            {
+                case Key.Left:
+                    args.Handled = true;
+                    SendKey((UIElement)args.Source, Key.Right);
+                    break;
+                case Key.Right:
+                    args.Handled = true;
+                    SendKey((UIElement)args.Source, Key.Left);
+                    break;
+            }
+        }
+        private void SendKey(UIElement sourceElement, Key keyToSend)
+        {
+            var args = new KeyEventArgs(InputManager.Current.PrimaryKeyboardDevice,
+                PresentationSource.FromVisual(sourceElement), 0, keyToSend);
+            args.RoutedEvent = Keyboard.KeyDownEvent;
+            InputManager.Current.ProcessInput(args);
+        }
+    }
+}
