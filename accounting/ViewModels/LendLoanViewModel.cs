22640f0f6adb6015bf4ee506f3b56b906f90455a
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:31:50 +0430

    Create LendLoan ViewModel

---
+++ accounting/ViewModels/LendLoanViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using accounting.Commands;
+using accounting.Models;
+
+namespace accounting.ViewModels
+{
+    public class LendLoanViewModel : BaseViewModel
+    {
+        private readonly List<AccountsItemsViewModel> _accountsItemsViewModels = new();
+        private readonly InvestmentFundModel _investmentFundModel;
+        private ObservableCollection<AccountsItemsViewModel> _accountList;
+        private string? _accountOwnerFullName;
+        private int? _amount;
+        private int? _fundAccountId;
+        private int? _instalmentCount;
+        private string? _personalAccountNumber;
+        private string? _searchText;
+
+        public LendLoanViewModel(InvestmentFundModel investmentFundModel)
+        {
+            _investmentFundModel = investmentFundModel;
+            LendLoanCommand = new LendLoanCommand(this, investmentFundModel);
+            _accountList = AccountsList;
+#pragma warning disable CS4014
+            GetAccounts();
+#pragma warning restore CS4014
+        }
+
+        public ICommand LendLoanCommand { get; }
+
+        public string? PersonalAccountNumber
+        {
+            get => _personalAccountNumber;
+            set => SetProperty(ref _personalAccountNumber, value);
+        }
+
+        public int? Amount
+        {
+            get => _amount;
+            set => SetProperty(ref _amount, value);
+        }
+
+        public int? FundAccountId
+        {
+            get => _fundAccountId;
+            set => SetProperty(ref _fundAccountId, value);
+        }
+
+        public string? SearchText
+        {
+            get => _searchText;
+            set
+            {
+                SetProperty(ref _searchText, value);
+                try
+                {
+                    FundAccountId = int.Parse(value!);
+                }
+                catch
+                {
+                    FundAccountId = null;
+                }
+
+                FilterAccountsList();
+            }
+        }
+
+        public ObservableCollection<AccountsItemsViewModel> AccountsList
+        {
+            get => _accountList;
+            set => SetProperty(ref _accountList, value);
+        }
+
+        public string? AccountOwnerFullName
+        {
+            get => _accountOwnerFullName;
+            set => SetProperty(ref _accountOwnerFullName, value);
+        }
+
+        public int? InstalmentCount
+        {
+            get => _instalmentCount;
+            set => SetProperty(ref _instalmentCount, value);
+        }
+
+        private void FilterAccountsList()
+        {
+            AccountsList = new ObservableCollection<AccountsItemsViewModel>(_accountsItemsViewModels);
+            if (SearchText != null)
+                foreach (var accountsItemsViewModel in _accountsItemsViewModels)
+                    if (!accountsItemsViewModel.AccountOwnerFullName.Contains(SearchText) &&
+                        !accountsItemsViewModel.AccountId.ToString().Contains(SearchText) &&
+                        !accountsItemsViewModel.AccountOwnerNationalId.Contains(SearchText))
+                        AccountsList.Remove(accountsItemsViewModel);
+        }
+
+        private async Task GetAccounts()
+        {
+            var peoplesAccounts = await _investmentFundModel.GetAllPeoplesAccounts();
+            AccountsList = ConvertToItemViewModelList(peoplesAccounts);
+        }
+
+        private ObservableCollection<AccountsItemsViewModel> ConvertToItemViewModelList(
+            Dictionary<PeoplesModel, IEnumerable<AccountsModel>>? peoplesAccounts)
+        {
+            foreach (var vPeoples in peoplesAccounts!)
+            foreach (var account in vPeoples.Value)
+            {
+                var accountsItemsViewModels =
+                    new AccountsItemsViewModel(account.Id, vPeoples.Key.Name + " " + vPeoples.Key.LastName,
+                        vPeoples.Key.NationalId);
+                _accountsItemsViewModels.Add(accountsItemsViewModels);
+            }
+
+            return new ObservableCollection<AccountsItemsViewModel>(_accountsItemsViewModels);
+        }
+    }
+}
