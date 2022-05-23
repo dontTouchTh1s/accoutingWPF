6f960cd68fddbd09895acdbda89aa53eabd60486
dontTouchTh1s <alim201381@gmail.com>
2022-05-23 22:36:47 +0430

    Change error string form type

--- accounting/Exceptions/NationalIdExistException.cs
+++ accounting/Exceptions/NationalIdExistException.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace accounting.Exceptions
 {
-    class NationalIdExistException : Exception
+    public class NationalIdExistException : Exception
     {
-        public NationalIdExistException() { }
-        public NationalIdExistException(string nationalId) : base(string.Format("People with national id {0} currently exist", nationalId))
-        { }
+        public NationalIdExistException()
+        {
+        }
+
+        public NationalIdExistException(string nationalId) : base(
+            $"People with national id {nationalId} already exist")
+        {
+            NationalId = nationalId;
+        }
+
+        public string NationalId { get; }
     }
 }
