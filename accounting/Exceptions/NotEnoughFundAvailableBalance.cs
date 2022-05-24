ae1e9dc8b4fe44c03bcb214eee28c61ef7781345
dontTouchThis <alim201381@gmail.com>
2022-05-24 12:17:05 +0430

    Exception for when balance dont have enough available balance for loans

---
+++ accounting/Exceptions/NotEnoughFundAvailableBalance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace accounting.Exceptions
+{
+    public class NotEnoughFundAvailableBalance : Exception
+    {
+        public long AvailableBalance;
+        public NotEnoughFundAvailableBalance(long availableBalance) : base("Not enough available balance in fund")
+        {
+            AvailableBalance = availableBalance;
+        }
+    }
+}
