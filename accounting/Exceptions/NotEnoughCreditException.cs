0d572b87ebe016cddec8a2c2a3db8c3d4854fa7b
dontTouchThis <alim201381@gmail.com>
2022-05-24 12:17:31 +0430

    Exception for when account dont have enough credit for lend a loan

---
+++ accounting/Exceptions/NotEnoughCreditException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace accounting.Exceptions
+{
+    public class NotEnoughCreditException : Exception
+    {
+        public int Credit;
+        public int MaximumLoan;
+        
+        public NotEnoughCreditException(int credit) : base("Not enough credit")
+        {
+            Credit = credit;
+            MaximumLoan = credit * 2;
+        }
+    }
+}
