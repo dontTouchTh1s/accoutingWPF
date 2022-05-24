a665dfe24d759ae648f646fd1aeaf8a94fe9ee45
dontTouchThis <alim201381@gmail.com>
2022-05-24 12:16:34 +0430

    Exception for when account credit is not enough on withdraws

---
+++ accounting/Exceptions/NotEnoughAvailableCreditException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace accounting.Exceptions
+{
+    public class NotEnoughAvailableCreditException : Exception
+    {
+        public int AvailableCredit;
+        public NotEnoughAvailableCreditException(int availableCredit) : base($"Not Enough credit, available credit: {availableCredit}")
+        {
+            AvailableCredit = availableCredit;
+        }
+    }
+}
