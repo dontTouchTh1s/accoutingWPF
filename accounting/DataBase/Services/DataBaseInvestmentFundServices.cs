fe247791127758991e1f9b8489751174adec87b9
dontTouchThis <alim201381@gmail.com>
2022-05-24 12:15:04 +0430

    Create new method to get fund available balance Change LendLoan method to reduce amount of loan first from each account minimum credit

--- accounting/DataBase/Services/DataBaseInvestmentFundServices.cs
+++ accounting/DataBase/Services/DataBaseInvestmentFundServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using accounting.DataBase.DbContexts;
+using accounting.Exceptions;
 using accounting.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,12 @@ namespace accounting.DataBase.Services
         {
             await using var context = _investmentFundDbContextFactory.CreateDbContext();
             var accountsCredit = await context.Accounts.Select(r => r.Credit).ToListAsync();
+            return accountsCredit.Sum();
+        }
+        public async Task<int> GetAvailableBalance()
+        {
+            await using var context = _investmentFundDbContextFactory.CreateDbContext();
+            var accountsCredit = await context.Accounts.Select(r => r.AvailableCredit).ToListAsync();
             return accountsCredit.Sum();
         }
 
@@ -78,27 +85,52 @@ namespace accounting.DataBase.Services
             foreach (var people in peoples) peoplesAccounts.Add(people, await people.GetAllAccounts());
             return peoplesAccounts;
         }
-
+        /// <summary>
+        ///     Add a loan to database loan table and reduce the amount of loan, from each account available credit.
+        /// <param name="loanModel">Model of loan</param>
+        /// </summary>        
         public async Task LendLoad(LoanModel loanModel)
         {
             await using var context = _investmentFundDbContextFactory.CreateDbContext();
-            var fundBalance = Convert.ToDouble(await GetBalance());
+            var fundAvailableBalance = Convert.ToDouble(await GetAvailableBalance());
+            // If fund didnt have enough available credit to lend the loan, throw
+            if (fundAvailableBalance < loanModel.Amount)
+                throw new NotEnoughFundAvailableBalance(Convert.ToInt64(fundAvailableBalance));
             var loanAmount = Convert.ToDouble(loanModel.Amount);
-            if (loanModel.Amount > fundBalance)
-                return;
-            if (loanModel.Amount > fundBalance / 2)
-                return;
-            var amountPercentPerAccount = loanAmount * 100 / fundBalance;
+            var loanAccount = await context.Accounts.Where(ac => ac.AccountId == loanModel.AccountId).FirstOrDefaultAsync();
+            // Amount of the loan can be twice of account credit. If it's not, throw
+            if (loanAmount > loanAccount.Credit * 2)
+                throw new NotEnoughCreditException(loanAccount.Credit);
+            // First, reduce amount of loan from each account available credit
+            // We can reduce up to 500000 (Minimum credit)
+            var accountsCount = await context.Accounts.CountAsync();
+            var amountPerAccount = Convert.ToInt32(loanAmount / accountsCount);
+            var minimumAmountPerAccount = amountPerAccount;
+            if (amountPerAccount > InvestmentFundModel.MinimumCredit)
+                minimumAmountPerAccount =  InvestmentFundModel.MinimumCredit;
             foreach (var account in context.Accounts)
             {
-                var availableCredit = Convert.ToDouble(account.AvailableCredit);
-                var loanAmountForAccount = Convert.ToInt32(availableCredit / 100 * amountPercentPerAccount);
-                account.AvailableCredit -= loanAmountForAccount;
+                account.AvailableCredit -= minimumAmountPerAccount;
+            }
+            // If any amount of loan remains, reduce from accounts that have available credit
+            var remainAmount = Convert.ToInt32(loanAmount - (minimumAmountPerAccount * accountsCount));
+            if (remainAmount != 0)
+            {
+                var amountPercentPerAccount = remainAmount * 100 / fundAvailableBalance;
+                foreach (var account in context.Accounts)
+                {
+                    var availableCredit = Convert.ToDouble(account.AvailableCredit);
+                    if (availableCredit == 0) break;
+                    var loanAmountForAccount = Convert.ToInt32(availableCredit / 100 * amountPercentPerAccount);
+                    account.AvailableCredit -= loanAmountForAccount;
+                }
             }
 
             var loanDTO = _dtoConverterService.LoanModelToDTO(loanModel);
             context.Loans.Add(loanDTO);
             await context.SaveChangesAsync();
         }
+
+        
     }
 }
