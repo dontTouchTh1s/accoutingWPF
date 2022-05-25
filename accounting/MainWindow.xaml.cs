a318e264c751b7245f60eda72fbfdc4101df127b
dontTouchThis <alim201381@gmail.com>
2022-05-25 11:09:26 +0430

    Remove unused codes

--- accounting/MainWindow.xaml.cs
+++ accounting/MainWindow.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System.Threading.Tasks;
-using System.Windows.Input;
-using System.Windows.Media;
-using accounting.ViewModels.Dialogs;
-using MaterialDesignThemes.Wpf;
-
-namespace accounting
+﻿namespace accounting
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
@@ -14,20 +8,6 @@ namespace accounting
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private async Task Animation_Label()
-        {
-            //await Task.Delay(3000);
-            //var animation = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
-            //LblMessage.BeginAnimation(OpacityProperty, animation);
-        }
-
-        private async void Control_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            var dataContext =
-                new MessageDialogViewModel("hello world", PackIconKind.About, new SolidColorBrush(Colors.Aqua));
-            await DialogHost.Show(dataContext, "rootDialog");
         }
     }
 }
