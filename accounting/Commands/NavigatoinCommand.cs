98263c10072c20f83e775fef963862730d4e3f25
dontTouchTh1s <alim201381@gmail.com>
2022-05-30 13:28:33 +0430

    Add Navigation command

---
+++ accounting/Commands/NavigatoinCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using accounting.Store;
+
+namespace accounting.Commands
+{
+    public class NavigatoinCommand : BaseCommand
+    {
+        private readonly Func<bool> _createViewModel;
+        private readonly NavigationService _navigationStore;
+
+        public NavigatoinCommand(NavigationService navigationStore, Func<bool> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _createViewModel();
+        }
+    }
+}
