e996884f0a61b66a0b737ed83d4828d1a452c3c7
dontTouchTh1s <alim201381@gmail.com>
2022-05-30 13:32:30 +0430

    Add command for updating each view model, when tabItem changed

---
+++ accounting/Commands/CheckTabItemUpdateCommand.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows.Controls;
+using accounting.Store;
+using accounting.ViewModels;
+
+namespace accounting.Commands
+{
+    public class CheckTabItemUpdateCommand : BaseCommand
+    {
+        private readonly MainViewModel _mainViewModel;
+        private readonly NavigationService _navigatoinSerivce;
+
+        public CheckTabItemUpdateCommand(MainViewModel mainViewModel, NavigationService navigationService)
+        {
+            _navigatoinSerivce = navigationService;
+            _mainViewModel = mainViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var args = (SelectionChangedEventArgs)parameter!;
+            try
+            {
+                var selectedItem = (TabItem)args.AddedItems[0]!;
+
+                switch (selectedItem.Name)
+                {
+                    case "Home":
+                    {
+                        _mainViewModel.SummeryViewModel.UpdateContent();
+                        break;
+                    }
+                    case "CreateAccount":
+                    {
+                        _mainViewModel.CreateAccountViewModel.UpdateContent();
+                        break;
+                    }
+                    case "Transactions":
+                    {
+                        _mainViewModel.TransactionsViewModel.UpdateContent();
+                        break;
+                    }
+                    case "ManageLoans":
+                    {
+                        _navigatoinSerivce.DepositLandViewModel.UpdateContent();
+                        _navigatoinSerivce.LendLoanViewModel.UpdateContent();
+                        break;
+                    }
+                    case "PeoplesAndAcconuts":
+                    {
+                        //
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+}
