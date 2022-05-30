64ec66e20ccb69b31d8140aa713671021b3ff3f2
dontTouchTh1s <alim201381@gmail.com>
2022-05-30 13:34:18 +0430

    Add navigation service to change current view without making a new oen, this just look like a tabControl i think

---
+++ accounting/Store/NavigationService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using accounting.Models;
+using accounting.ViewModels;
+
+namespace accounting.Store
+{
+    public class NavigationService
+    {
+        private readonly InvestmentFundModel _investmentFundModel;
+
+        private BaseViewModel _manageLoansCurrentViewModel;
+
+        public NavigationService(InvestmentFundModel investmentFundModel)
+        {
+            _investmentFundModel = investmentFundModel;
+            DepositLandViewModel = new DepositLoanInstalmentViewModel(_investmentFundModel);
+            LendLoanViewModel = new LendLoanViewModel(_investmentFundModel, this);
+            NavigateToLendLoan();
+        }
+
+        public BaseViewModel ManageLoansCurrentViewModel
+        {
+            get => _manageLoansCurrentViewModel;
+            set
+            {
+                _manageLoansCurrentViewModel = value;
+                OnCurrentViewChanged();
+            }
+        }
+
+        public LendLoanViewModel LendLoanViewModel { get; set; }
+        public DepositLoanInstalmentViewModel DepositLandViewModel { get; set; }
+
+        private void OnCurrentViewChanged()
+        {
+            CurrentViewChanged?.Invoke();
+        }
+
+        public void NavigateToDepositLoan()
+        {
+            ManageLoansCurrentViewModel = DepositLandViewModel;
+        }
+
+        public void NavigateToLendLoan()
+        {
+            ManageLoansCurrentViewModel = LendLoanViewModel;
+        }
+
+        public event Action CurrentViewChanged;
+    }
+}
