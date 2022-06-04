296c509f9cccafb65b924cb3fd79f740d5c5110a
dontTouchTh1s <alim201381@gmail.com>
2022-06-04 23:30:45 +0430

    Add UpdateContent method to update all of viewModels in manageLoan tabControls

--- accounting/ViewModels/ManageLoans/ManageLoanViewModel.cs
+++ accounting/ViewModels/ManageLoans/ManageLoanViewModel.cs
@@ -1,21 +1,26 @@
-﻿using System.Windows.Input;
-using accounting.Commands;
-using accounting.Models;
-using accounting.Store;
+﻿using accounting.Models;
 
-namespace accounting.ViewModels
+namespace accounting.ViewModels.ManageLoans
 {
     public class ManageLoanViewModel : BaseViewModel
     {
-
         public ManageLoanViewModel(InvestmentFundModel investmentFundModel)
         {
             LendLoanViewModel = new LendLoanViewModel(investmentFundModel);
             DepositLoanInstalmentViewModel = new DepositLoanInstalmentViewModel(investmentFundModel);
-
+            ViewLoanViewModle = new ViewLoansViewModel(investmentFundModel);
         }
 
         public LendLoanViewModel LendLoanViewModel { get; }
         public DepositLoanInstalmentViewModel DepositLoanInstalmentViewModel { get; }
+
+        public ViewLoansViewModel ViewLoanViewModle { get; }
+
+        public sealed override void UpdateContent()
+        {
+            LendLoanViewModel.UpdateContent();
+            DepositLoanInstalmentViewModel.UpdateContent();
+            ViewLoanViewModle.UpdateContent();
+        }
     }
 }
