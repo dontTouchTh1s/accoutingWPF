754fb6f8daa6c87e5c470dba8bac7da78362682e
dontTouchTh1s <alim201381@gmail.com>
2022-06-04 18:33:15 +0430

    Remove unused e parameter

--- accounting/Commands/CreateAccountCommand.cs
+++ accounting/Commands/CreateAccountCommand.cs
@@ -54,7 +54,7 @@ namespace accounting.Commands
                         PackIconKind.WarningCircle, new SolidColorBrush(Colors.Red));
                 await DialogHost.Show(dialogViewModel, "rootDialog");
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 var dialogViewModel =
                     new MessageDialogViewModel("در ایجاد حساب مشکلی پیش آمده است.",
