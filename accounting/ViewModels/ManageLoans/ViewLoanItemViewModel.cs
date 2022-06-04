00eaa0992eaf617007f79bebdaa8f6179939b6ab
dontTouchTh1s <alim201381@gmail.com>
2022-06-04 23:29:18 +0430

    Move all about lona files to ManageLoan directory

---
+++ accounting/ViewModels/ManageLoans/ViewLoanItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace accounting.ViewModels.ManageLoans
+{
+    public class ViewLoanItemViewModel
+    {
+        public ViewLoanItemViewModel(ushort id, string amount, byte instalmentCount,
+            string ownerFullName, ushort accountId,
+            string lendDate, string? personalAccountNumber)
+        {
+            Id = id;
+            Amount = amount;
+            InstalmentCount = instalmentCount;
+            OwnerFullName = ownerFullName;
+            AccountId = accountId;
+            LendDate = lendDate;
+            PersonalAccountNumber = personalAccountNumber ?? "وارد نشده";
+        }
+
+        public ushort Id { get; }
+        public string Amount { get; }
+        public byte InstalmentCount { get; }
+        public string OwnerFullName { get; }
+        public ushort AccountId { get; }
+        public string LendDate { get; }
+        public string PersonalAccountNumber { get; }
+    }
+}
---
+++ accounting/Views/ManageLoans/ManageLoanView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace accounting.Views
+{
+    public partial class ManageLoanView : UserControl
+    {
+        public ManageLoanView()
+        {
+            InitializeComponent();
+        }
+    }
+}
