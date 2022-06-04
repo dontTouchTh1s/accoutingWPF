727395b6b7cae82dea3c59425371ab7908e8a3ec
dontTouchThis <alim201381@gmail.com>
2022-06-04 18:16:47 +0430

    ViewLoanViewModel initialize

---
+++ accounting/ViewModels/ViewLoansViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using accounting.Models;
+
+namespace accounting.ViewModels
+{
+    public class ViewLoansViewModel
+    {
+        public ViewLoansViewModel(InvestmentFundModel investmentFundModel)
+        {
+            
+        }
+
+        public ICommand UpdateDataCommand { get; }
+        ObservableCollection<>
+    }
+}
