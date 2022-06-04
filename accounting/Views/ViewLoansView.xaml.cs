e0e5036634debb66437ed425dee42d2c8bcfd875
dontTouchThis <alim201381@gmail.com>
2022-06-04 18:16:11 +0430

    Add viewLoanView to view dataGrid of loans

---
+++ accounting/Views/ViewLoansView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace accounting.Views
+{
+    public partial class ViewLoansView : UserControl
+    {
+        public ViewLoansView()
+        {
+            InitializeComponent();
+        }
+    }
+}
