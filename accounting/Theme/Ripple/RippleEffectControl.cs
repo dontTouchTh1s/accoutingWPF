24a862082d7be3b20c66e64e634fabbbb169b84c
dontTouchTh1s <alim201381@gmail.com>
2022-06-04 20:01:13 +0430

    Change class type to public, fix some bugs

--- accounting/Theme/Ripple/RippleEffectControl.cs
+++ accounting/Theme/Ripple/RippleEffectControl.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,11 +8,23 @@ using System.Windows.Shapes;
 
 namespace accounting.Theme.Ripple
 {
-    class RippleEffectControl : ContentControl
+    public class RippleEffectControl : ContentControl
     {
+        private const string ChildName = "PART_grid";
+
+        // Using a DependencyProperty as the backing store for HighlightBackground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HighlightBackgroundProperty =
+            DependencyProperty.Register("HighlightBackground", typeof(Brush), typeof(RippleEffectControl),
+                new PropertyMetadata(Brushes.White));
+
+        private Storyboard? _animation;
+        private Ellipse? _ellipse;
+        private Grid? _grid;
+
         static RippleEffectControl()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(RippleEffectControl), new FrameworkPropertyMetadata(typeof(RippleEffectControl)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RippleEffectControl),
+                new FrameworkPropertyMetadata(typeof(RippleEffectControl)));
         }
 
         public Brush HighlightBackground
@@ -25,25 +33,15 @@ namespace accounting.Theme.Ripple
             set => SetValue(HighlightBackgroundProperty, value);
         }
 
-        private const string ChildName = "PART_grid";
-
-        // Using a DependencyProperty as the backing store for HighlightBackground.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty HighlightBackgroundProperty =
-            DependencyProperty.Register("HighlightBackground", typeof(Brush), typeof(RippleEffectControl), new PropertyMetadata(Brushes.White));
-        private Ellipse? _ellipse;
-        private Grid? _grid;
-        private Storyboard? _animation;
         public override void OnApplyTemplate()
         {
-
-
             base.OnApplyTemplate();
 
             _ellipse = (Ellipse)GetTemplateChild("PART_ellipse");
             _grid = (Grid)GetTemplateChild(ChildName);
-            _animation = (Storyboard)_grid.FindResource("PART_animation");
+            _animation = (Storyboard)_grid.FindResource("PartAnimation");
 
-            this.AddHandler(MouseDownEvent, new RoutedEventHandler((sender, e) =>
+            AddHandler(MouseDownEvent, new RoutedEventHandler((sender, e) =>
             {
                 var targetWidth = Math.Max(ActualWidth, ActualHeight) * 2;
                 var mousePosition = ((MouseButtonEventArgs)e).GetPosition(this);
@@ -54,7 +52,8 @@ namespace accounting.Theme.Ripple
                 ((DoubleAnimation)_animation.Children[0]).To = targetWidth;
                 //set the to and from values of the animation that animates the distance relative to the container (grid)
                 ((ThicknessAnimation)_animation.Children[1]).From = startMargin;
-                ((ThicknessAnimation)_animation.Children[1]).To = new Thickness(mousePosition.X - targetWidth / 2, mousePosition.Y - targetWidth / 2, 0, 0);
+                ((ThicknessAnimation)_animation.Children[1]).To = new Thickness(mousePosition.X - targetWidth / 2,
+                    mousePosition.Y - targetWidth / 2, 0, 0);
                 _ellipse.BeginStoryboard(_animation);
             }), true);
         }
