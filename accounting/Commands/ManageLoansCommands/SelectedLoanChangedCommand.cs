81d1d3095c985df1b52c8aaec8622988b0d3c966
dontTouchTh1s <alim201381@gmail.com>
2022-06-06 23:45:32 +0430

    Add selectionChangedCommand to change text prefix of currency textfileds

---
+++ accounting/Commands/ManageLoansCommands/SelectedLoanChangedCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using accounting.ViewModels.ManageLoans;
+
+namespace accounting.Commands.ManageLoansCommands
+{
+    public class SelectedLoanChangedCommand : BaseCommand
+    {
+        private readonly DepositLoanInstalmentViewModel _depositLoanInstalmentViewModel;
+
+        public SelectedLoanChangedCommand(DepositLoanInstalmentViewModel depositLoanInstalmentViewModel)
+        {
+            _depositLoanInstalmentViewModel = depositLoanInstalmentViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var args = (SelectionChangedEventArgs)parameter!;
+            if (args.AddedItems.Count != 0)
+            {
+                var item = (ViewLoanItemViewModel)args.AddedItems[0]!;
+                _depositLoanInstalmentViewModel.AmountHelperText =
+                    string.Format("حداقل میزان قسط {0} تومان است.", item.MinimumInstalmentAmount.ToString());
+            }
+        }
+    }
+}
