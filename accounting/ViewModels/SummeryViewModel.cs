d8af8a7797eeedab2fa82756a9fd482468fc73d6
dontTouchTh1s <alim201381@gmail.com>
2022-06-06 13:01:28 +0430

    Add AvailableBalance prop to show fund available balance

--- accounting/ViewModels/SummeryViewModel.cs
+++ accounting/ViewModels/SummeryViewModel.cs
@@ -5,6 +5,7 @@ namespace accounting.ViewModels
     public class SummeryViewModel : BaseViewModel
     {
         private readonly InvestmentFundModel _investmentFundModel;
+        private string _availableBalance;
         private ulong _balance;
 
         public SummeryViewModel(InvestmentFundModel investmentFundModel)
@@ -19,14 +20,26 @@ namespace accounting.ViewModels
             set => SetProperty(ref _balance, value);
         }
 
-        public async void GetBalance()
+        public string AvailableBalance
+        {
+            get => _availableBalance;
+            set => SetProperty(ref _availableBalance, value);
+        }
+
+        private async void GetBalance()
         {
             Balance = await _investmentFundModel.GetBalance();
+        }
+
+        private async void GetAvailableBalance()
+        {
+            AvailableBalance = (await _investmentFundModel.GetAvailableBalance()).ToString();
         }
 
         public sealed override void UpdateContent()
         {
             GetBalance();
+            GetAvailableBalance();
         }
     }
 }
