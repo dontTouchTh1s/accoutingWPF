5170cb522374dd2b29b167b385090232213f83fc
dontTouchTh1s <alim201381@gmail.com>
2022-06-06 12:59:32 +0430

    Add SearchText full prob to search in loan by full name and loan amount

--- accounting/ViewModels/ManageLoans/ViewLoansViewModel.cs
+++ accounting/ViewModels/ManageLoans/ViewLoansViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using accounting.Models;
@@ -9,6 +10,8 @@ namespace accounting.ViewModels.ManageLoans
     {
         private readonly InvestmentFundModel _investmentFundModel;
         private ObservableCollection<ViewLoanItemViewModel> _loansList;
+        private string _serachText;
+        private List<ViewLoanItemViewModel> _viewLoanItemViewModelsList = new();
 
         public ViewLoansViewModel(InvestmentFundModel investmentFundModel)
         {
@@ -22,25 +25,47 @@ namespace accounting.ViewModels.ManageLoans
             set => SetProperty(ref _loansList, value);
         }
 
-        public async void GetLoans()
+        public string SearchText
+        {
+            get => _serachText;
+            set
+            {
+                SetProperty(ref _serachText, value);
+                FilterLoans(value);
+            }
+        }
+
+        private void FilterLoans(string filterValue)
+        {
+            LoansList = new ObservableCollection<ViewLoanItemViewModel>(_viewLoanItemViewModelsList);
+            foreach (var loanItem in _viewLoanItemViewModelsList.Where(loanItem =>
+                         !loanItem.OwnerFullName.Contains(filterValue) &&
+                         !loanItem.Amount.Contains(filterValue)))
+                LoansList.Remove(loanItem);
+        }
+
+        private async void GetLoans()
         {
             LoansList = new ObservableCollection<ViewLoanItemViewModel>();
             var loansDic = await _investmentFundModel.GetAllLoans();
             foreach (var people in loansDic)
             foreach (var account in people.Value)
             foreach (var loans in account.Value)
-                    {
-                        var loanItem = new ViewLoanItemViewModel(loans.Id,
-                            loans.Amount.ToString(),
-                            loans.InstallmentsCount,
-                            people.Key.Name + " " + people.Key.LastName,
-                            account.Key.Id,
-                            loans.LendDate.ToString(CultureInfo.CurrentCulture),
-                            loans.PersonalAccountNumber
-                        );
-                        LoansList.Add(loanItem);
-                    }
-            LoansList = new ObservableCollection<ViewLoanItemViewModel>(LoansList.OrderBy(model => model.OwnerFullName));
+            {
+                var loanItem = new ViewLoanItemViewModel(loans.Id,
+                    loans.Amount.ToString(),
+                    loans.InstallmentsCount,
+                    people.Key.Name + " " + people.Key.LastName,
+                    account.Key.Id,
+                    loans.LendDate.ToString(CultureInfo.CurrentCulture),
+                    loans.PersonalAccountNumber
+                );
+                LoansList.Add(loanItem);
+            }
+
+            LoansList = new ObservableCollection<ViewLoanItemViewModel>(LoansList.OrderBy(model =>
+                model.OwnerFullName));
+            _viewLoanItemViewModelsList = new List<ViewLoanItemViewModel>(LoansList);
         }
 
         public sealed override void UpdateContent()
