f25122d988d80f8cebbdd1923d0e1c0953779a6a
dontTouchTh1s <alim201381@gmail.com>
2022-06-08 23:39:06 +0430

    Add MinimumInstalmentAmount to show minimum amount of each instalment. Code cleanup, change amount type to string to show in currency format

--- accounting/ViewModels/ComboBoxItems/LoanItemViewModel.cs
+++ accounting/ViewModels/ComboBoxItems/LoanItemViewModel.cs
@@ -1,17 +1,22 @@
-﻿namespace accounting.ViewModels.ComboBoxItems
+﻿using System.Globalization;
+
+namespace accounting.ViewModels.ComboBoxItems
 {
     public class LoanItemViewModel
     {
         public LoanItemViewModel(ushort id, ulong amount, byte instalmentCount)
         {
             Id = id;
-            Amount = amount;
+            Amount = amount.ToString("N0", CultureInfo.CurrentCulture);
             InstalmentCount = instalmentCount;
+            MinimumInstalmentAmount = (amount / instalmentCount).ToString("N0", CultureInfo.CurrentCulture);
         }
 
-        public ushort Id { get;}
-        public ulong Amount { get;}
-        public byte InstalmentCount { get;}
+        public ushort Id { get; }
+        public string Amount { get; }
+        public byte InstalmentCount { get; }
+        public string MinimumInstalmentAmount { get; }
+
 
         public override string ToString()
         {
