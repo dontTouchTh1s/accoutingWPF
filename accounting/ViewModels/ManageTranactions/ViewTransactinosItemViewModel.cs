4358708ca89f97a355122fd4ad94ec01941126f6
dontTouchTh1s <alim201381@gmail.com>
2022-06-08 23:21:04 +0430

    Initilize, show transactions info in ViewTransaction

---
+++ accounting/ViewModels/ManageTranactions/ViewTransactinosItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace accounting.ViewModels.ManageTranactions
+{
+    public class ViewTransactinosItemViewModel
+    {
+        public ViewTransactinosItemViewModel(ushort id, string ownerFullName, long amount, ushort accountId,
+            DateTime date, string? personalAccountNumber)
+        {
+            Id = id;
+            OwnerFullName = ownerFullName;
+            Amount = Math.Abs(amount).ToString("N0", CultureInfo.CurrentCulture);
+            AccountId = accountId;
+            Date = date.ToString(CultureInfo.CurrentCulture);
+            PersonalAccountNumber = personalAccountNumber;
+            Type = amount < 0 ? "برداشت" : "واریز";
+        }
+
+        public ushort Id { get; }
+        public string OwnerFullName { get; }
+        public string Amount { get; }
+        public ushort AccountId { get; }
+        public string Date { get; }
+        public string Type { get; }
+        public string? PersonalAccountNumber { get; }
+    }
+}
---
+++ accounting/Views/ManageTransactions/ViewTransactionsItemView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace accounting.Views.ManageTransactions
+{
+    public partial class ViewTransactionsItemView : UserControl
+    {
+        public ViewTransactionsItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
