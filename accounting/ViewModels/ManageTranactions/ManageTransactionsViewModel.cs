d035210e1487aa70a3ebd898fe8f276e93bcfc4b
dontTouchTh1s <alim201381@gmail.com>
2022-06-08 23:13:10 +0430

    Initialize

---
+++ accounting/ViewModels/ManageTranactions/ManageTransactionsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using accounting.Models;
+
+namespace accounting.ViewModels.ManageTranactions
+{
+    public class ManageTransactionsViewModel : BaseViewModel
+    {
+        public ManageTransactionsViewModel(InvestmentFundModel investmentFundModel)
+        {
+            TransactionsViewModel = new TransactionsViewModel(investmentFundModel);
+            ViewTransactionsViewModel = new ViewTransactionsViewModel(investmentFundModel);
+        }
+
+        public TransactionsViewModel TransactionsViewModel { get; }
+        public ViewTransactionsViewModel ViewTransactionsViewModel { get; }
+
+        public override void UpdateContent()
+        {
+            TransactionsViewModel.UpdateContent();
+            ViewTransactionsViewModel.UpdateContent();
+        }
+    }
+}
