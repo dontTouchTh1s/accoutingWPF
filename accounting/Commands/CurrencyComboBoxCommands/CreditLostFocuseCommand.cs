e4329d5bcc3902285bd9911877d9af5705299cde
dontTouchTh1s <alim201381@gmail.com>
2022-06-09 15:05:35 +0430

    Add another constructor to minimum instalment amount, change execute method to return amount value to minimum, if its lower

--- accounting/Commands/CurrencyComboBoxCommands/CreditLostFocuseCommand.cs
+++ accounting/Commands/CurrencyComboBoxCommands/CreditLostFocuseCommand.cs
@@ -1,10 +1,19 @@
-﻿using accounting.ViewModels.ManageAccounts;
+﻿using System.Globalization;
+using accounting.Models;
+using accounting.ViewModels.ManageAccounts;
+using accounting.ViewModels.ManageLoans;
 
 namespace accounting.Commands.CurrencyComboBoxCommands
 {
     public class CreditLostFocusCommand : BaseCommand
     {
         private readonly CreateAccountViewModel _createAccountViewModel;
+        private readonly InstalmentLoanViewModel? _instalmentLoanViewModel;
+
+        public CreditLostFocusCommand(InstalmentLoanViewModel instalmentLoanViewModel)
+        {
+            _instalmentLoanViewModel = instalmentLoanViewModel;
+        }
 
         public CreditLostFocusCommand(CreateAccountViewModel createAccountViewModel)
         {
@@ -13,8 +22,15 @@ namespace accounting.Commands.CurrencyComboBoxCommands
 
         public override void Execute(object? parameter)
         {
-            if (_createAccountViewModel.Credit < 500000)
-                _createAccountViewModel.CreditView = "500000";
+            if (_instalmentLoanViewModel != null)
+            {
+                if (_instalmentLoanViewModel.Amount < _instalmentLoanViewModel.CurrentLoanMinimumInstalment)
+                    _instalmentLoanViewModel.AmountView =
+                        _instalmentLoanViewModel.CurrentLoanMinimumInstalment.ToString();
+                return;
+            }
+            if (_createAccountViewModel.Credit < InvestmentFundModel.MinimumCredit)
+                    _createAccountViewModel.CreditView = "500000";
         }
     }
 }
