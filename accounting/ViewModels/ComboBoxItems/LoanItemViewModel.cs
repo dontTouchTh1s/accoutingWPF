51fb125b53e5cd504f7ff64ce7d42fef3d320773
dontTouchTh1s <alim201381@gmail.com>
2022-06-12 23:07:57 +0430

    Add remained amount, not used, maybe remove

--- accounting/ViewModels/ComboBoxItems/LoanItemViewModel.cs
+++ accounting/ViewModels/ComboBoxItems/LoanItemViewModel.cs
@@ -4,18 +4,20 @@ namespace accounting.ViewModels.ComboBoxItems
 {
     public class LoanItemViewModel
     {
-        public LoanItemViewModel(ushort id, ulong amount, byte instalmentCount)
+        public LoanItemViewModel(ushort id, ulong amount, byte instalmentCount, ulong remainedAmount)
         {
             Id = id;
             Amount = amount.ToString("N0", CultureInfo.CurrentCulture);
             InstalmentCount = instalmentCount;
             MinimumInstalmentAmount = (amount / instalmentCount).ToString("N0", CultureInfo.CurrentCulture);
+            RemainedAmount = remainedAmount.ToString("N0", CultureInfo.CurrentCulture);
         }
 
         public ushort Id { get; }
         public string Amount { get; }
         public byte InstalmentCount { get; }
         public string MinimumInstalmentAmount { get; }
+        public string RemainedAmount { get; }
 
 
         public override string ToString()
