2bd88b9804353cf2057213d06f7bc859d42c78da
dontTouchTh1s <alim201381@gmail.com>
2022-06-12 23:01:37 +0430

    Move to exceptions namespace, change AvailableBalance to ulong

---
+++ accounting/Exceptions/NotEnoughFundAvailableBalanceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace accounting.Exceptions
+{
+    public class NotEnoughFundAvailableBalanceException : Exception
+    {
+        public readonly ulong AvailableBalance;
+
+        public NotEnoughFundAvailableBalanceException(ulong availableBalance) : base(
+            "Not enough available balance in fund")
+        {
+            AvailableBalance = availableBalance;
+        }
+    }
+}
