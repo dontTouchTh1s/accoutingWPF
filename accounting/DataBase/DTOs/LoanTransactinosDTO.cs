30a52789294ba33f0f6a8a0ac521ec6713bc90ab
dontTouchTh1s <alim201381@gmail.com>
2022-06-12 22:20:40 +0430

    Add loanTransaction for saving available credit change in lending loan process.

---
+++ accounting/DataBase/DTOs/LoanTransactinosDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace accounting.DataBase.DTOs
+{
+    public class LoanTransactinosDTO
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public ushort Id { get; set; }
+
+        public long Amount { get; init; }
+        public string Date { get; init; }
+        public ushort LoanId { get; init; }
+        public ushort AccountId { get; init; }
+        [ForeignKey("AccountId")] public AccountDTO Account { get; set; }
+
+        public string? PersonalAccountNumber { get; init; }
+    }
+}
---
+++ accounting/Models/LoanTransactinosModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace accounting.Models
+{
+    public class LoanTransactinosModel
+    {
+        public LoanTransactinosModel(long amount, ushort loanId, ushort accountId, string? personalAccountNumber)
+        {
+            Amount = amount;
+            LoanId = loanId;
+            Date = DateTime.Now;
+            AccountId = accountId;
+            PersonalAccountNumber = personalAccountNumber;
+        }
+
+        public LoanTransactinosModel(ushort id, long amount, DateTime date, ushort loanId, ushort accountId,
+            string? personalAccountNumber)
+        {
+            Id = id;
+            Amount = amount;
+            Date = date;
+            LoanId = loanId;
+            AccountId = accountId;
+            PersonalAccountNumber = personalAccountNumber;
+        }
+
+
+        public ushort Id { get; set; }
+        public long Amount { get; init; }
+        public DateTime Date { get; init; }
+        public ushort LoanId { get; init; }
+        public ushort AccountId { get; init; }
+        public string? PersonalAccountNumber { get; init; }
+    }
+}
