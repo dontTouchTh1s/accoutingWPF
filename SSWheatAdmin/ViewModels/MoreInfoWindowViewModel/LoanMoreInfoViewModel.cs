3d81a30574654734a7b36336d9f7efcb73c50b88
dontTouchTh1s <alim201381@gmail.com>
2022-06-16 10:53:33 +0430

    Change fullProps to get properties and remove fields

--- SSWheatAdmin/ViewModels/MoreInfoWindowViewModel/LoanMoreInfoViewModel.cs
+++ SSWheatAdmin/ViewModels/MoreInfoWindowViewModel/LoanMoreInfoViewModel.cs
@@ -11,86 +11,46 @@ namespace SSWheatAdmin.ViewModels.MoreInfoWindowViewModel
     {
         private readonly InvestmentFundModel _investmentFundModel;
 
-        private string _instalmentAmount;
-
-        private byte _instalmentCount;
-
         private ObservableCollection<InstalmentLoanModel> _instalmentList;
 
-        private string _lendDate;
-
-        private string _loanAmount;
-
         private string _nextInstalmentDate;
-
-        private ushort _ownerAccountId;
-
-        private string _ownerFullName;
 
         private ulong _payedAmount;
 
         private byte _payedInstalmentCount;
+        private ushort _id;
 
         public LoanMoreInfoViewModel(InvestmentFundModel investmentFundModel, ViewLoanItemViewModel loan,
             ushort windowId) : base(windowId)
         {
             _investmentFundModel = investmentFundModel;
-            LoanId = loan.Id;
+            LoanId = $"({loan.Id})";
+            _id = loan.Id;
             OwnerFullName = loan.OwnerFullName;
-            LendDate = loan.LendDate;
-            InstalmentCount = loan.InstalmentCount;
-            OwnerAccountId = loan.AccountId;
+            LendDate = DateTime.Parse(loan.LendDate).Date.ToShortDateString();
             LoanAmount = loan.Amount;
+            InstalmentCount = loan.InstalmentCount;
+            InstalmentAmount =
+                (ulong.Parse(LoanAmount, NumberStyles.Number, CultureInfo.CurrentCulture) / Convert.ToUInt64(InstalmentCount)).ToString("N0",
+                    CultureInfo.CurrentCulture);
+            OwnerAccountId = loan.AccountId;
+
             UpdateContent();
         }
 
-        public byte InstalmentCount
-        {
-            get => _instalmentCount;
-            set => SetProperty(ref _instalmentCount, value);
-        }
+        public byte InstalmentCount { get; }
 
-        public ObservableCollection<InstalmentLoanModel> InstalmentList
-        {
-            get => _instalmentList;
-            private set => SetProperty(ref _instalmentList, value);
-        }
+        public ObservableCollection<InstalmentLoanModel> InstalmentList { get; set; }
 
-        public string OwnerFullName
-        {
-            get => _ownerFullName;
-            set => SetProperty(ref _ownerFullName, value);
-        }
+        public string OwnerFullName { get; }
 
-        public ushort OwnerAccountId
-        {
-            get => _ownerAccountId;
-            set => SetProperty(ref _ownerAccountId, value);
-        }
+        public ushort OwnerAccountId { get; }
 
-        public string LendDate
-        {
-            get => _lendDate;
-            set => SetProperty(ref _lendDate, value);
-        }
+        public string LendDate { get; }
 
-        public string LoanAmount
-        {
-            get => _loanAmount;
-            set => SetProperty(ref _loanAmount, value);
-        }
+        public string LoanAmount { get; }
 
-        public byte InstalemntCount
-        {
-            get => _instalmentCount;
-            set => SetProperty(ref _instalmentCount, value);
-        }
-
-        public string InstalmentAmount
-        {
-            get => _instalmentAmount;
-            set => SetProperty(ref _instalmentAmount, value);
-        }
+        public string InstalmentAmount { get; }
 
         public string NextInstalmentDate
         {
@@ -110,19 +70,19 @@ namespace SSWheatAdmin.ViewModels.MoreInfoWindowViewModel
             private set => SetProperty(ref _payedInstalmentCount, value);
         }
 
-        public ushort LoanId { get; }
+        public string LoanId { get; }
 
         private async void GetInstalments()
         {
-            var instalments = await _investmentFundModel.GetLoanInstalments(LoanId);
+            var instalments = await _investmentFundModel.GetLoanInstalments(_id);
             var instalmentLoanModels = instalments.ToList();
+            //instalmentLoanModels.Select(inst => inst.Date.ToString(CultureInfo.CurrentCulture)).ToList();
             InstalmentList = new ObservableCollection<InstalmentLoanModel>(instalmentLoanModels);
             if (!instalmentLoanModels.Any())
                 return;
             PayedAmount = InstalmentList.Select(inst => inst.Amount).Aggregate((a, b) => a + b);
-            PayedInstalemtCount = Convert.ToByte(_instalmentList.Count());
-            var date = DateTime.Parse(LendDate, CultureInfo.CurrentCulture);
-            NextInstalmentDate = date.AddMonths(PayedInstalemtCount + 1).ToString(CultureInfo.CurrentCulture);
+            PayedInstalemtCount = Convert.ToByte(InstalmentList.Count());
+            NextInstalmentDate = DateTime.Parse(LendDate).Date.AddMonths(PayedInstalemtCount + 1).ToShortDateString();
         }
 
         public sealed override void UpdateContent()
