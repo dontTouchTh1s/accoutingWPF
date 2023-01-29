d203069ea0fc322de606fcb95583af1d65180b2c
ALIM <alim201381@gmail.com>
2023-01-29 18:38:16 +0330

    Make windows movable, and add close and minimize buttons

--- SSWheatAdmin/Commands/ManageLoansCommands/LoanSelectedCommand.cs
+++ SSWheatAdmin/Commands/ManageLoansCommands/LoanSelectedCommand.cs
@@ -13,12 +13,13 @@ namespace SSWheatAdmin.Commands.ManageLoansCommands
         {
             _investmentFundModel = investmentFundModel;
         }
-
+          
         public override void Execute(object? parameter)
         {
             if (parameter == null) return;
             var args = (SelectionChangedEventArgs)parameter;
-            if (args.AddedItems?[0] == null) return;
+            if (args.AddedItems.Count == 0) return;
+            if (args.AddedItems[0] == null) return;
             var item = args.AddedItems[0] as ViewLoanItemViewModel;
             var loanMoreInfoViewModel = new LoanMoreInfoViewModel(_investmentFundModel, item, item.Id);
             App.WindowsNavigationServices.OpenMoreInfoWindow(loanMoreInfoViewModel);
---
+++ SSWheatAdmin/Views/MoreInfoWindow/AccountMoreInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SSWheatAdmin.Views.MoreInfoWindow
+{
+    /// <summary>
+    /// Interaction logic for AccountMoreInfo.xaml
+    /// </summary>
+    public partial class AccountMoreInfo : Window
+    {
+        public AccountMoreInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
